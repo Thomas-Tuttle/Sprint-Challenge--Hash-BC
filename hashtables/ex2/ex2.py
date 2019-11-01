c9e11122a64f290691d65364b3c7f00c24080eb5
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-11-01 13:27:44 -0400

    Mined coins and finished ex1 & ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,14 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+#############################################################################################################
+    for i in range(length):
+        if tickets[i].source == "NONE":
+            route[0] = tickets[i].destination
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
 
+    for r in range(length):
+        if route[r-1] is not None:
+            route[r] = hash_table_retrieve(hashtable, route[r-1])
+#############################################################################################################
     return route
